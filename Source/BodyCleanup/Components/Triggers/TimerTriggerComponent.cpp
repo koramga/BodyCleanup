749bc80528e8c75d6c0544090ab6a43e9f330c75
koramga <koramga@gmail.com>
2022-01-14 23:43:27 +0900

    TimerTrigger 구현 완료

---
+++ Source/BodyCleanup/Components/Triggers/TimerTriggerComponent.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimerTriggerComponent.h"
+
+UTimerTriggerComponent::UTimerTriggerComponent()
+{
+	bIsCanControlTriggerComponentFromType = false;
+	TriggerComponentFromType = ETriggerComponentFromType::Action;
+}
+
+void UTimerTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay(); 
+	
+
+	GetOwner()->GetWorldTimerManager().SetTimer(TimerHandle, this, &UTimerTriggerComponent::__AdvanceTimer, CycleTime, true);
+}
+
+void UTimerTriggerComponent::__AdvanceTimer()
+{
+	CallTriggerObservers(true);
+
+	UE_LOG(LogTemp, Display, TEXT("Advance Timer"));
+
+	if (false == bIsLoopCycle)
+	{		
+		GetOwner()->GetWorldTimerManager().ClearTimer(TimerHandle);
+	}
+}
---
+++ Source/BodyCleanup/Components/Triggers/TimerTriggerComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../TriggerComponent.h"
+#include "TimerTriggerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BODYCLEANUP_API UTimerTriggerComponent : public UTriggerComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTimerTriggerComponent();
+
+protected:
+	bool			bIsTriggerDestroy = false;
+	FTimerHandle	TimerHandle;
+
+	//주기에 맞게 지속적으로 Trigger를 호출할지 여부입니다.
+	UPROPERTY(EditAnywhere)
+	bool			bIsLoopCycle = false;
+
+	//주기를 설정합니다.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float			CycleTime = 1.f;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private :
+	void __AdvanceTimer();
+};
