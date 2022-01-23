b89738ee7f91ff8c8aa93cf3124d69b8e093f244
koramga <koramga@gmail.com>
2022-01-23 17:54:45 +0900

    MetaTools 추가 / MetaTools에 VariableMetaData 추가 완료

--- Plugins/MetaTools/Source/MetaTools/MetaTools.Build.cs
+++ Plugins/MetaTools/Source/MetaTools/MetaTools.Build.cs
@@ -2,9 +2,9 @@
 
 using UnrealBuildTool;
 
-public class LevelDesignerTools : ModuleRules
+public class MetaTools : ModuleRules
 {
-	public LevelDesignerTools(ReadOnlyTargetRules Target) : base(Target)
+	public MetaTools(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
@@ -27,7 +27,6 @@ public class LevelDesignerTools : ModuleRules
 			{
 				"Core",
 				// ... add other public dependencies that you statically link with here ...
-				"MetaTools",
 			}
 			);
 			
