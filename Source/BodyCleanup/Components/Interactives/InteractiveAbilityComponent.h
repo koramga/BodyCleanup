c281b3384b8ca69141c9d0a4f4548ca294c5ac4f
koramga <koramga@gmail.com>
2022-01-28 11:53:05 +0900

    추가 해야 할 Interactive 미리 추가
    InteractiveAbility : 능력치 조정
    InteractiveScript : 대사 출력
    InteractiveSucking : Sucking 상호 작용
    InteractiveObject : ('E'키를 눌러 상호작용)

---
+++ Source/BodyCleanup/Components/Interactives/InteractiveAbilityComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../InteractiveComponent.h"
+#include "InteractiveAbilityComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BODYCLEANUP_API UInteractiveAbilityComponent : public UInteractiveComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BodyCleanup/Components/Interactives/InteractiveScriptComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../InteractiveComponent.h"
+#include "InteractiveScriptComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BODYCLEANUP_API UInteractiveScriptComponent : public UInteractiveComponent
+{
+	GENERATED_BODY()
+	
+};
