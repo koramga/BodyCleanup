7acc5392803eaf9e3844a631e9d2ca1174c75716
koramga <koramga@gmail.com>
2022-02-01 17:40:01 +0900

    Update Sound2DTriggerComponent.h

--- Plugins/LevelDesignerTools/Source/LevelDesignerTools/Trigger/Classes/Actor/Sound2DTriggerComponent.h
+++ Plugins/LevelDesignerTools/Source/LevelDesignerTools/Trigger/Classes/Actor/Sound2DTriggerComponent.h
@@ -17,16 +17,19 @@ class LEVELDESIGNERTOOLS_API USound2DTriggerComponent : public UTriggerActorComp
 public :
 	USound2DTriggerComponent();
 
-	protected :
+protected :
+	//Sound Wave는 Resource를 지정해야합니다.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Setup|Sound2DTriggerComponent")
 	USoundWave*		SoundWave;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Debug|Sound2DTriggerComponent")
 	UAudioComponent*	AudioComponent;
 
+	//시작 시, Fade In Time을 고려하여 시작합니다. 0이하의 값은 Fade In을 활용하지 않겠다는 의미가 됩니다. 기준은 초(Sec) 입니다.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Setup|Sound2DTriggerComponent")
 	float			FadeInTime = 0.f;
 
+	//종료 시, Fade Out Time을 고려하여 종료합니다. 0이하의 값은 Fade Out을 활용하지 않겠다는 의미가 됩니다. 기준은 초(Sec) 입니다.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Setup|Sound2DTriggerComponent")
 	float			FadeOutTime = 1.f;
 
