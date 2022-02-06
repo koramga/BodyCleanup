4e166bed9851c309502739f34bae3757c2b8d142
koramga <koramga@gmail.com>
2022-02-06 18:07:47 +0900

    Montage를 실행시킬 수 있는 간단한 Ability 추가

---
+++ Source/BodyCleanup/GCS/Ability/BaseCAPAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CAPAbility.h"
+#include "BaseCAPAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BODYCLEANUP_API UBaseCAPAbility : public UCAPAbility
+{
+	GENERATED_BODY()
+	
+};
