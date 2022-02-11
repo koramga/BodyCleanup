298fb12473117b7a1ba4660d578fc2246ea10f1f
koramga <koramga@gmail.com>
2022-02-11 13:37:13 +0900

    SoundDesignerTools 최조제작 (Plugin으로 제작 예정)
    SoundDesigner가 쉽게 게임 내에 사운드를 넣기 위한 도구 제작

---
+++ Plugins/SoundDesignerTools/Source/SoundDesignerTools/Public/AnimNotify/AnimNotify_SoundTool.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_SoundTool.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOUNDDESIGNERTOOLS_API UAnimNotify_SoundTool : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
---
+++ Plugins/SoundDesignerTools/Source/SoundDesignerTools/Public/SoundDesignerToolsDefines.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SoundDesignerToolsDefines.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FSoundWave2DInfo
+{
+	GENERATED_BODY();
+};
+
+
+USTRUCT(BlueprintType)
+struct FSoundWave3DInfo
+{
+	GENERATED_BODY();
+};
+
+
+USTRUCT(BlueprintType)
+struct FSoundWaveAttachInfo
+{
+	GENERATED_BODY();
+};
