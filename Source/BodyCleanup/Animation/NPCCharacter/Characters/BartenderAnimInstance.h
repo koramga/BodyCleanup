d9fda73a7911a7dc77df8d37784c16879f67abe4
koramga <koramga@gmail.com>
2022-02-12 07:22:25 +0900

    Bartender 추가

---
+++ Source/BodyCleanup/Animation/NPCCharacter/Characters/BartenderAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../NPCCharacterAnimInstance.h"
+#include "BartenderAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BODYCLEANUP_API UBartenderAnimInstance : public UNPCCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BodyCleanup/Character/NPCCharacter/Characters/BartenderCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../BaseNPCCharacter.h"
+#include "BartenderCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BODYCLEANUP_API ABartenderCharacter : public ABaseNPCCharacter
+{
+	GENERATED_BODY()
+	
+};
