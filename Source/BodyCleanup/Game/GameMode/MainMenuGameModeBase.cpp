1b9cce4aea634497970e7cec6b07920275ed2096
koramga <koramga@gmail.com>
2022-02-16 23:04:18 +0900

    MainMenu 작업 완료

--- Source/BodyCleanup/Game/GameMode/MainMenuGameModeBase.cpp
+++ Source/BodyCleanup/Game/GameMode/MainMenuGameModeBase.cpp
@@ -4,6 +4,7 @@
 #include "MainMenuGameModeBase.h"
 
 #include "BodyCleanup/UI/Screen/MainMenuScreenWidget.h"
+#include "Camera/CameraActor.h"
 #include "Kismet/GameplayStatics.h"
 
 void AMainMenuGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
@@ -25,5 +26,17 @@ void AMainMenuGameModeBase::BeginPlay()
 	if(IsValid(PlayerController))
 	{
 		PlayerController->SetShowMouseCursor(true);
-	}	
+	}
+
+	TArray<AActor*> Actors;
+	
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), Actors);
+
+	if(Actors.Num() > 0)
+	{
+		if(IsValid(Actors[0]))
+		{
+			PlayerController->SetViewTargetWithBlend(Actors[0], 0.f);
+		}		
+	}
 }
