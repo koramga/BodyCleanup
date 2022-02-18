03234ccb4bc27b1dc2730cd1171ee47e8b1a9a65
koramga <koramga@gmail.com>
2022-02-18 21:56:34 +0900

    Script 기본 샘플형 완성

--- Source/BodyCleanup/Components/Interactive/Classes/InteractiveScriptComponent.cpp
+++ Source/BodyCleanup/Components/Interactive/Classes/InteractiveScriptComponent.cpp
@@ -2,6 +2,9 @@
 
 
 #include "InteractiveScriptComponent.h"
+#include "../../../Game/GameInstance/BaseGameInstance.h"
+#include "../../../Game/GameMode/MainGameModeBase.h"
+#include "../../../Data/TableRow/TableRows.h"
 
 UInteractiveScriptComponent::UInteractiveScriptComponent()
 {
@@ -23,6 +26,20 @@ void UInteractiveScriptComponent::UpdateTrigger(const FLevelTriggerUpdateParam& 
 		//여기서부터 시작합니다.
 		//일단 여기가 제대로 들어오는지 봅시다.
 
-		
+		AMainGameModeBase* MainGameModeBase = Cast<AMainGameModeBase>(GetWorld()->GetAuthGameMode());
+
+		if(IsValid(MainGameModeBase))
+		{
+			UBaseGameInstance* BaseGameInstance = Cast<UBaseGameInstance>(MainGameModeBase->GetGameInstance());
+
+			if(IsValid(BaseGameInstance))
+			{
+				TArray<FScriptTableRow> ScriptTableRows;
+				
+				BaseGameInstance->GetScripts(ScriptTableRows, SceneName);
+
+				//Script는 받아진다. 이 상태에서 이제 시작된다. 모든 포커스는 여기로 잡힐거기때문에 
+			}
+		}
 	}
 }
